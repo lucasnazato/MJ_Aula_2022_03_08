53bf6f521f518df78fab819dd70b6e4faef2ab9b
Aluno <lucas.nazato@hotmail.com>
2022-04-26 22:29:52 -0300

    Alterações nickname

--- Assets/Script/LookAtCamera.cs
+++ Assets/Script/LookAtCamera.cs
@@ -1,9 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using TMPro;
 
 public class LookAtCamera : MonoBehaviour
 {
+    PhotonView view;
+    public TMP_Text txtName;
+
+    private void Start()
+    {
+        view = GetComponentInParent<PhotonView>();
+
+        if (view.IsMine)
+        {
+            txtName.enabled = false;
+        }
+    }
+
     private void Update()
     {
         transform.LookAt(Camera.main.transform);
